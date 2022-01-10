c0e25ee4f7431a4d2f876c97abce334af9d11276
Ajay Patidar <62458035+devajaypatidar@users.noreply.github.com>
2022-01-10 19:04:57 +0530

    Add files via upload
    The first upload of the dice game

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+var head = document.querySelector('#heading');
+
+var img1 = document.querySelector('.img1');
+var img2 = document.querySelector('.img2');
+
+head.addEventListener('click', function(e) {
+
+    var randomNumber1 = Math.random() * 6;
+    randomNumber1 = Math.floor(randomNumber1+ 1);
+    
+    
+    var randomNumber2 = Math.random() * 6;
+    randomNumber2 = Math.floor(randomNumber2+ 1);
+    
+
+    if(randomNumber1>randomNumber2)
+    {
+        head.textContent = "🚩Player 1 Wins  ";
+    }else if(randomNumber1<randomNumber2)
+    {
+        head.textContent = "Player 2 wins 🚩";
+    }else
+    {
+        head.textContent = "Draw Match 🏁";
+    }
+
+    img1.src = `images/dice${randomNumber1}.png`;
+    img2.src = `images/dice${randomNumber2}.png`;
+
+});
